c97e761b2587ebff302497a64bb6dc043a23c9ee
michaelyuhe <2312744987@qq.com>
2023-12-07 20:25:05 +0800

    fix: do not batch group after create or update

--- src/background.ts
+++ src/background.ts
@@ -1,7 +1,9 @@
-import { batchGroupTabs } from "./services";
+import { handleOneTab } from "./services";
 import { getStorage } from "./utils";
 
 export const TYPES = ["Develop", "Entertainment", "Reading", "Social"];
+
+const tabMap = new Map<string, number>();
 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   const result = message.result;
@@ -9,35 +11,95 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   TYPES.forEach((_, i) => groupOneType(result[i].type, result[i].tabIds));
 });
 
+chrome.tabGroups.onCreated.addListener((group) => {
+  if (!group.title) return;
+  const type = group.title;
+
+  if (tabMap.has(type)) {
+    return;
+  }
+
+  tabMap.set(type, group.id);
+});
+
 async function groupOneType(type: string, tabIds: number[]) {
   if (tabIds.length === 0) return;
-  chrome.tabs.group({ tabIds: tabIds }, async (groupId) => {
+
+  console.log(tabIds);
+
+  chrome.tabs.group({ tabIds }, async (groupId) => {
     await chrome.tabGroups.update(groupId, { title: type });
   });
 }
 
-function handleNewTab(tab: chrome.tabs.Tab) {
-  getStorage<string>('openai_key').then(openAIKey => {
-      chrome.tabs.query({ currentWindow: true }).then(tabs => {
-          batchGroupTabs(tabs, TYPES, openAIKey).then(result => {
-            TYPES.forEach((_, i) => groupOneType(result[i].type, result[i].tabIds as number[]));
-          });
+async function handleNewTab(tab: chrome.tabs.Tab) {
+  getStorage<string>("openai_key")
+    .then(async (openAIKey) => {
+      if (!tab.id) return;
+
+      const type = await handleOneTab(tab, openAIKey);
+      const groupId = tabMap.get(type);
+
+      if (!groupId) {
+        console.log("No group id found for type:", type);
+
+        chrome.tabs.group({ tabIds: [tab.id] }, async (groupId) => {
+          await chrome.tabGroups.update(groupId, { title: type });
+        });
+
+        return;
+      }
+
+      const groupTabIds = await chrome.tabs
+        .query({
+          groupId: groupId,
+        })
+        .then((tabs) => tabs.map((tab) => tab.id));
+
+      const tabIds = [tab.id, ...(groupTabIds as number[])];
+
+      chrome.tabs.group({ tabIds: tabIds }, async (groupId) => {
+        await chrome.tabGroups.update(groupId, { title: type });
       });
-  }).catch(error => {
-      console.error('Error in handleNewTab:', error);
-  });
+    })
+    .catch((error) => {
+      console.error("Error in handleNewTab:", error);
+    });
 }
 
 function handleTabUpdate(tabId: any, changeInfo: any, tab: any) {
-  if (changeInfo.status === 'complete') {
-      getStorage<string>('openai_key').then(openAIKey => {
-          chrome.tabs.query({ currentWindow: true }).then(tabs => {
-              batchGroupTabs(tabs, TYPES, openAIKey).then(result => {
-                  TYPES.forEach((_, i) => groupOneType(result[i].type, result[i].tabIds as number[]));
-              });
+  if (changeInfo.status === "complete") {
+    getStorage<string>("openai_key")
+      .then(async (openAIKey) => {
+        if (!tab.id) return;
+
+        const type = await handleOneTab(tab, openAIKey);
+        const groupId = tabMap.get(type);
+
+        if (!groupId) {
+          console.log("No group id found for type:", type);
+
+          chrome.tabs.group({ tabIds: [tab.id] }, async (groupId) => {
+            await chrome.tabGroups.update(groupId, { title: type });
           });
-      }).catch(error => {
-          console.error('Error in handleTabUpdate:', error);
+
+          return;
+        }
+
+        const groupTabIds = await chrome.tabs
+          .query({
+            groupId: groupId,
+          })
+          .then((tabs) => tabs.map((tab) => tab.id));
+
+        const tabIds = [tab.id, ...(groupTabIds as number[])];
+
+        chrome.tabs.group({ tabIds: tabIds }, async (groupId) => {
+          await chrome.tabGroups.update(groupId, { title: type });
+        });
+      })
+      .catch((error) => {
+        console.error("Error in handleUpdateTab:", error);
       });
   }
 }
--- src/services.ts
+++ src/services.ts
@@ -1,7 +1,9 @@
-type TabGroup = {
+import { TYPES } from "./background";
+
+interface TabGroup {
   type: string;
   tabIds: (number | undefined)[];
-};
+}
 
 export async function batchGroupTabs(
   tabs: chrome.tabs.Tab[],
@@ -15,12 +17,14 @@ export async function batchGroupTabs(
       url: tab.url,
     };
   });
+
   const result: TabGroup[] = types.map((type) => {
     return {
       type,
       tabIds: [],
     };
   });
+
   try {
     await Promise.all(
       tabInfoList.map(async (tab) => {
@@ -58,9 +62,43 @@ export async function batchGroupTabs(
         result[index].tabIds.push(tab.id);
       })
     );
-    return result
+    return result;
   } catch (error) {
     console.error(error);
-    return result
+    return result;
+  }
+}
+
+export async function handleOneTab(tab: chrome.tabs.Tab, openAIKey: string) {
+  try {
+    const response = await fetch("https://api.openai.com/v1/chat/completions", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${openAIKey}`,
+      },
+      body: JSON.stringify({
+        messages: [
+          {
+            role: "system",
+            content: `You are an assistant to help decide a type in ${TYPES.join(
+              ", "
+            )} for a tab by search and analyze its url. Response the type only. Do not return anything else.`,
+          },
+          {
+            role: "user",
+            content: `The site url is ${tab.url}`,
+          },
+        ],
+        model: "gpt-3.5-turbo",
+      }),
+    });
+
+    const data = await response.json();
+    const type = data.choices[0].message.content;
+
+    return type;
+  } catch (error) {
+    console.log(error);
   }
 }
