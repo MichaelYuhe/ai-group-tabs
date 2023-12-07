44030f173cc1c0ce8f8112b3b5236ed8aba5c0b9
observedobserver <270001151@qq.com>
2023-12-06 22:48:12 -0800

    feat: support regroup on update/create

--- src/background.ts
+++ src/background.ts
@@ -1,3 +1,6 @@
+import { batchGroupTabs } from "./services";
+import { getStorage } from "./utils";
+
 export const TYPES = ["Develop", "Entertainment", "Reading", "Social"];
 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
@@ -12,3 +15,32 @@ async function groupOneType(type: string, tabIds: number[]) {
     await chrome.tabGroups.update(groupId, { title: type });
   });
 }
+
+function handleNewTab(tab: chrome.tabs.Tab) {
+  getStorage<string>('openai_key').then(openAIKey => {
+      chrome.tabs.query({ currentWindow: true }).then(tabs => {
+          batchGroupTabs(tabs, TYPES, openAIKey).then(result => {
+            TYPES.forEach((_, i) => groupOneType(result[i].type, result[i].tabIds as number[]));
+          });
+      });
+  }).catch(error => {
+      console.error('Error in handleNewTab:', error);
+  });
+}
+
+function handleTabUpdate(tabId: any, changeInfo: any, tab: any) {
+  if (changeInfo.status === 'complete') {
+      getStorage<string>('openai_key').then(openAIKey => {
+          chrome.tabs.query({ currentWindow: true }).then(tabs => {
+              batchGroupTabs(tabs, TYPES, openAIKey).then(result => {
+                  TYPES.forEach((_, i) => groupOneType(result[i].type, result[i].tabIds as number[]));
+              });
+          });
+      }).catch(error => {
+          console.error('Error in handleTabUpdate:', error);
+      });
+  }
+}
+
+chrome.tabs.onCreated.addListener(handleNewTab);
+chrome.tabs.onUpdated.addListener(handleTabUpdate);
