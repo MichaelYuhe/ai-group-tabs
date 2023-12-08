f9c578007bf6fa311987e0bcc81989b7060c634e
Yuhang <2312744987@qq.com>
2023-12-08 21:17:23 +0800

    Update src/popup.tsx

--- src/popup.tsx
+++ src/popup.tsx
@@ -79,7 +79,7 @@ const Popup = () => {
               }}
             />
             <button className="rounded-md w-fit bg-indigo-600 px-2.5 py-1.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
-              Add New Group Type
+              Add
             </button>
           </div>
         </form>
