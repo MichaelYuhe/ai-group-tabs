a674d693cccdd09a8dce7d4c776f3f60bb1b2a8b
michaelyuhe <2312744987@qq.com>
2023-12-11 19:44:47 +0800

    style: disable button style

--- src/popup.tsx
+++ src/popup.tsx
@@ -130,7 +130,7 @@ const Popup = () => {
               disabled={!newType}
               className="rounded-md w-fit bg-primary/lg px-2.5 py-1.5 text-sm font-semibold 
             text-white shadow-sm hover:bg-primary focus-visible:outline focus-visible:outline-2 
-            focus-visible:outline-offset-2"
+            focus-visible:outline-offset-2 disabled:bg-primary/sm"
             >
               Add
             </button>
