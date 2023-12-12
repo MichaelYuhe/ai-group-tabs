0d7d84187b689f80f653b41b21be5264f348dd92
michaelyuhe <2312744987@qq.com>
2023-12-12 22:56:12 +0800

    refactor: options page

---
+++ src/components/Switch.tsx
@@ -0,0 +1,27 @@
+export default function Switch({
+  isChecked,
+  onChange,
+  text,
+}: {
+  isChecked: boolean;
+  onChange: () => void;
+  text: string;
+}) {
+  return (
+    <div className="flex items-center mt-2">
+      <label className="relative inline-flex cursor-pointer items-center">
+        <input
+          id="switch"
+          type="checkbox"
+          checked={isChecked}
+          className="peer sr-only"
+          onClick={onChange}
+        />
+        <label htmlFor="switch" className="hidden"></label>
+        <div className="peer h-6 w-11 rounded-full border bg-slate-200 after:absolute after:left-[2px] after:top-0.5 after:h-5 after:w-5 after:rounded-full after:border after:border-gray-300 after:bg-white after:transition-all after:content-[''] peer-checked:bg-primary peer-checked:after:translate-x-full peer-checked:after:border-white peer-focus:ring-green-300"></div>
+      </label>
+
+      <span className="ml-3 text-gray-900 text-sm">{text}</span>
+    </div>
+  );
+}
