5caf8443fbcbc9d7321b4906b6e4af4ec035f5a7
lawvs <18554747+lawvs@users.noreply.github.com>
2023-12-22 23:31:40 +0800

    fix: react warn

--- src/components/LoadingSpinner.tsx
+++ src/components/LoadingSpinner.tsx
@@ -12,7 +12,7 @@ export const LoadingSpinner = () => (
       cy="12"
       r="10"
       stroke="currentColor"
-      stroke-width="4"
+      strokeWidth="4"
     ></circle>
     <path
       className="opacity-75"
